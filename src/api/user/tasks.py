3522c49295f66e347fbe8e433dfa5f0b00839515
Abhishek <abhishekjha96500@gmail.com>
2025-09-06 18:53:04 +0530

    added tasks template.

---
+++ src/api/user/tasks.py
@@ -0,0 +1,8 @@
+def create_user_task():
+    print("create_user_task")
+    pass
+
+
+def expire_user_task():
+    print("expire_user_task")
+    pass
--- src/config/celery.py
+++ src/config/celery.py
@@ -1,5 +1,15 @@
 from celery import Celery
+from celery.schedules import crontab
 from config.base import Settings
 
 
 app = Celery("ecosphere", broker=Settings.REDIS_URL)
+
+
+app.conf.beat_schedule = {
+    "add-every-30-seconds": {
+        "task": "tasks.add",
+        "schedule": crontab(hour=7, minute=30, day_of_week=1),
+        "args": (16, 16),
+    },
+}
