9f1e98ea1e86451f49a3a83038d0377ca93d17c6
Abhishek <abhishekjha96500@gmail.com>
2025-09-06 23:53:16 +0530

    added code for firebase auth.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
 
+import firebase_admin
+from firebase_admin import credentials
 
 from config.base import settings
 
@@ -15,6 +17,9 @@ app = FastAPI(
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
 )
+
+cred = credentials.Certificate(settings.FIREBASE_CONFIG)
+firebase_admin.initialize_app(cred)
 
 # Set all CORS enabled origins
 if settings.all_cors_origins:
